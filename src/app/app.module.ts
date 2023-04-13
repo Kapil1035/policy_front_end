64ef5457f14870124b140dbd2f373726f29a8693
Manjeet Nandal <manjeetnandal1@gmail.com>
2023-04-13 11:01:19 +0530

    init commits

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppRoutingModule } from './app-routing.module';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { AlertComponent } from './_components';
+import { HomeComponent } from './home';
+import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent
+,
+        LandingComponent ,
+        HeaderComponent ,
+        FooterComponent ,
+        MainContentComponent ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { };
---
+++ src/app/users/users-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LayoutComponent } from './layout.component';
+import { ListComponent } from './list.component';
+import { AddEditComponent } from './add-edit.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { CasesComponent } from './cases/cases.component';
+import { InfoComponent } from './info/info.component';
+
+const routes: Routes = [
+    {
+        path: '', component: LayoutComponent,
+        children: [
+            { path: '', component: ListComponent },
+            { path: 'getQuotes', component: QuotesComponent },
+            { path: 'cases', component: CasesComponent },
+            { path: 'add', component: AddEditComponent },
+            { path: 'edit/:id', component: AddEditComponent },
+            { path: 'info', component: InfoComponent }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class UsersRoutingModule { }
