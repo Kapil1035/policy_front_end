ea884f57cd112067c9bc3acaf72109bfba300346
Manjeet Nandal <manjeetnandal1@gmail.com>
2023-04-13 10:58:47 +0530

    fgfg

--- policyapp-frontend/src/app/users/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'any'
-})
-export class UserService {
-  data:any;
-  constructor() { }
-}
--- policyapp-frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://65.2.90.5:4000'
-};
